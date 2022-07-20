0a73304f7f124a926f6cba19d6dc8051157f8119
flugelhell <bleadhelsink@gmail.com>
2022-07-20 23:34:17 +0700

    update

--- training_sarinah/perpustakaan/__manifest__.py
+++ training_sarinah/perpustakaan/__manifest__.py
@@ -14,15 +14,16 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Test',
+    'category': 'Sales',
     'version': '0.1',
-
+    'application': True,
     # any module necessary for this one to work correctly, depends itu pake nama modules
     'depends': ['base', 'product'],
 
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'data/sequence.xml',
         'views/perpustakaan.xml',
     ],
     # only loaded in demonstration mode
--- training_sarinah/perpustakaan/models/peminjaman_buku.py
+++ training_sarinah/perpustakaan/models/peminjaman_buku.py
@@ -1,21 +1,23 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
-class PeminjamanBuka(models.Model):
+class PeminjamanBuku(models.Model):
     _name = 'peminjaman.buku'
     _description = 'Module Perpustakan'
 
-    name = fields.Char(string='Name')
-    tanggal_pinjam = fields.Date(string='Tanggal Pinjam')
+    name = fields.Char(string='Name', readonly=True,
+                       required=True, copy=False, default='New')
+    tanggal_pinjam = fields.Date(string='Tanggal Pinjam', required=True)
     tanggal_kembali = fields.Date(string='Tanggal Kembali')
-    peminjam_id = fields.Many2one('res.users', string='Peminjam')
+    peminjam_id = fields.Many2one(
+        'res.users', string='Peminjam', required=True)
     no_telp = fields.Char(string='No Telp')
     petugas_id = fields.Many2one('res.users', string='Petugas')
     daftar_buku_ids = fields.One2many(
         'daftar.buku', 'peminjaman_buku_id', string='Daftar Buku')
     total_harga_pinjam = fields.Float(string='Total Harga Pinjam')
     status = fields.Selection(
-        [('dipinjam', 'Dipinjam'), ('dikembalikan', 'Dikembalikan')])
+        [('draft', 'Draft'), ('dipinjam', 'Dipinjam'), ('dikembalikan', 'Dikembalikan')], default='draft')
 
     # state = fields.Selection([('draft', 'Draft'), ('inprogress', 'In progress'),
     #                          ('done', 'Done')], default='draft', string="Status")
@@ -26,6 +28,22 @@ class PeminjamanBuka(models.Model):
 
     # attendee_ids = fields.One2many(
     #     'training.attendees', 'training_id', string='Attendees')
+    def update_status(self):
+        if self.status == 'draft':
+            self.write({'status': 'dipinjam'})
+        elif self.status == 'dipinjam':
+            self.write({'status': 'dikembalikan'})
+        else:
+            self.write({'status': 'draft'})
+
+    # Membuat sequence, harus membuat record xml juga
+    @api.model
+    def create(self, vals):
+        if vals.get('name', 'New') == 'New':
+            vals['name'] = self.env['ir.sequence'].next_by_code(
+                'pinjam.buku.sequence') or 'New'
+        result = super(PeminjamanBuku, self).create(vals)
+        return result
 
 
 class DaftarBuku(models.Model):
@@ -33,14 +51,13 @@ class DaftarBuku(models.Model):
     _description = 'Daftar Buku'
 
     buku_id = fields.Many2one('product.product', string='Buku')
-    nama_buku = fields.Char(related='buku_id.name', string='Nama Buku')
     peminjaman_buku_id = fields.Many2one(
         'peminjaman.buku', string='Peminjam ID')
     harga_pinjam_buku = fields.Float(string='Harga Pinjam')
 
 
-# Menambahkan Field di product.product (ProductProduct dilihat dari module product odoo di bagian Model)
-class ProductProduct(models.Model):
+# Menambahkan Field di product.product (Product dilihat dari module product odoo di bagian Model)
+class Product(models.Model):
     _inherit = 'product.product'
 
     sinopsis = fields.Text(string='Sinopsis')
